1b60a78ac19b9d972821a4a3c2021e0c341d1236
Александр <@USER>
2022-12-25 19:48:35 +0300

    Сущности

---
+++ Data/AntroStop.DAL/Entities/Element.cs
@@ -0,0 +1,16 @@
+﻿using AntroStop.DAL.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AntroStop.DAL.Entities
+{
+    [Index(nameof(Id))]
+    public class Element : GuidEntity
+    {
+        public string Url { get; set; }
+        public string Type { get; set; }
+        public Violation Violation { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
---
+++ Data/AntroStop.DAL/Entities/Violation.cs
@@ -0,0 +1,24 @@
+﻿using AntroStop.DAL.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AntroStop.DAL.Entities
+{
+    [Index(nameof(Id))]
+    public class Violation : GuidEntity
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Coordinates { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public string Answer { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
