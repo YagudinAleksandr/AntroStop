4e5a78e0b74071f5d8cfa2c958ad6381bf79b3c4
Александр <@USER>
2022-12-25 19:47:30 +0300

    Интерфейсы репозиториев сущностей int и guid

---
+++ Services/AntroStop.Interfaces.Base/Repositories/IGuidRepository.cs
@@ -0,0 +1,19 @@
+﻿using AntroStop.Interfaces.Base.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AntroStop.Interfaces.Base.Repositories
+{
+    public interface IGuidRepository<T> where T : IGuidEntity
+    {
+        Task<bool> ExistID(Guid ID, CancellationToken Cancel = default);
+        Task<int> Count(CancellationToken Cancel = default);
+        Task<IEnumerable<T>> GetAll(CancellationToken Cancel = default);
+        Task<T> Add(T entity, CancellationToken Cancel = default);
+        Task<T> Update(T entity, CancellationToken Cancel = default);
+        Task<T> Delete(Guid ID, CancellationToken Cancel = default);
+        Task<T> Get(Guid ID, CancellationToken Cancel = default);
+    }
+}
---
+++ Services/AntroStop.Interfaces.Base/Repositories/IIntRepository.cs
@@ -0,0 +1,18 @@
+﻿using AntroStop.Interfaces.Base.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AntroStop.Interfaces.Base.Repositories
+{
+    public interface IIntRepository<T> where T : IIntEntity
+    {
+        Task<bool> ExistID(int ID, CancellationToken Cancel = default);
+        Task<int> Count(CancellationToken Cancel = default);
+        Task<IEnumerable<T>> GetAll(CancellationToken Cancel = default);
+        Task<T> Add(T entity, CancellationToken Cancel = default);
+        Task<T> Update(T entity, CancellationToken Cancel = default);
+        Task<T> Delete(int ID, CancellationToken Cancel = default);
+        Task<T> Get(int ID, CancellationToken Cancel = default);
+    }
+}
