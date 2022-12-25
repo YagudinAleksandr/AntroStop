f4dc8b75a9d0641992173e93201c8151500ab7a9
Александр <@USER>
2022-12-25 19:49:16 +0300

    Частичная реализация репозиториев

---
+++ Data/AntroStop.DAL/Repositories/ViolationRepository.cs
@@ -0,0 +1,47 @@
+﻿using AntroStop.DAL.Entities;
+using AntroStop.Interfaces.Base.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AntroStop.DAL.Repositories
+{
+    public class ViolationRepository<T> : IGuidRepository<T> where T : Violation, new()
+    {
+        public Task<T> Add(T entity, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Count(CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Delete(Guid ID, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistID(Guid ID, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(Guid ID, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll(CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
