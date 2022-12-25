9abf06facc8cac28cd2a3b30e23e2612bf9db0f8
Александр <@USER>
2022-12-25 19:48:56 +0300

    Создание контекста БД

---
+++ Data/AntroStop.DAL/Context/DataDB.cs
@@ -0,0 +1,21 @@
+﻿using AntroStop.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AntroStop.DAL.Context
+{
+    public class DataDB : DbContext
+    {
+        public DbSet<Violation> Violations { get; set; }
+        public DbSet<Element> Elements { get; set; }
+        public DataDB(DbContextOptions<DataDB> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Violation>()
+                .HasMany<Element>()
+                .WithOne(v => v.Violation)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
