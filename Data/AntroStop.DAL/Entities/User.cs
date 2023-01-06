8fbeb2458f07769879a6792f57ec0dd9bb65cd86
Александр Ягудин <yagudin.a@outlook.com>
2023-01-06 17:43:46 +0300

    Сущность пользователя

---
+++ Data/AntroStop.DAL/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using AntroStop.DAL.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace AntroStop.DAL.Entities
+{
+    [Index(nameof(Username))]
+    public class User : StringEntity
+    {
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public int RoleID { get; set; }
+    }
+}
