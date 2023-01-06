8c8e63bd34772596357ebaac31416c0e3970922b
Александр Ягудин <yagudin.a@outlook.com>
2023-01-06 17:43:29 +0300

    Базовая сущность строковых значений

---
+++ Data/AntroStop.DAL/Entities/Base/StringEntity.cs
@@ -0,0 +1,16 @@
+﻿using AntroStop.Interfaces.Base.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AntroStop.DAL.Entities.Base
+{
+    public class StringEntity : IStringEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Username { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.Now;
+    }
+}
