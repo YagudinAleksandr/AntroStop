97f5a3897b887dcd2cab9b954ee8a9056351641d
Александр Ягудин <yagudin.a@outlook.com>
2023-01-29 22:47:19 +0300

    Добавление моделей представления

---
+++ Common/AntroStop.Domain.Base/Models/ViolationsInfo.cs
@@ -0,0 +1,15 @@
+﻿using AntroStop.Interfaces.Base.Entities;
+using System;
+
+namespace AntroStop.Domain.Base.Models
+{
+    public class ViolationsInfo : IGuidEntity
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Description { get; set; }
+        public string Coordinates { get; set; }
+        public string Status { get; set; }
+    }
+}
