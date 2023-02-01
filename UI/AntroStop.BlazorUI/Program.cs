3ee6a1d681873132a482d2a65cbe284ffcfcb7f5
Александр Ягудин <yagudin.a@outlook.com>
2023-02-01 23:01:36 +0300

    Подключение сервисов

--- UI/AntroStop.BlazorUI/Program.cs
+++ UI/AntroStop.BlazorUI/Program.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using AntroStop.Interfaces.Base.Repositories;
+using AntroStop.Domain.Base.Models;
+using AntroStop.WebAPIClients.Repositories;
+using AntroStop.BlazorUI.Infrastructure.Extensions;
+using AntroStop.Interfaces.Repositories;
+using AntroStop.Domain.Base.Models.Users;
 
 namespace AntroStop.BlazorUI
 {
@@ -17,7 +19,14 @@ namespace AntroStop.BlazorUI
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var services = builder.Services;
+
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //services.AddHttpClient<IGuidRepository<ViolationsInfo>, WebViolationsRepository<ViolationsInfo>>(
+            //(host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress+"api/ViolationsRepository"));
+
+            services.AddApi<IViolationRepository<ViolationsInfo>, WebViolationsRepository<ViolationsInfo>>("api/ViolationsRepository/");
+            services.AddApi<IStringRepository<UsersInfo>, WebUsersRepository<UsersInfo>>("api/UsersRepository/");
 
             await builder.Build().RunAsync();
         }
