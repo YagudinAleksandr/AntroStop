5379e51a24512ed31145d436511b0c98f4c19a3e
Александр Ягудин <yagudin.a@outlook.com>
2023-02-01 23:00:01 +0300

    Реализация класса UserInfo

---
+++ Common/AntroStop.Domain.Base/Models/Users/UsersInfo.cs
@@ -0,0 +1,17 @@
+﻿using AntroStop.Interfaces.Base.Entities;
+using System;
+
+namespace AntroStop.Domain.Base.Models.Users
+{
+    public class UsersInfo : IStringEntity
+    {
+        public string ID { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int RoleID { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+        public bool Status { get; set; }
+    }
+}
