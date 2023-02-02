0e31999f238431a7d570b9cf7972c8030d07c49a
Александр Ягудин <yagudin.a@outlook.com>
2023-02-02 22:03:38 +0300

    Добавлена модель представления ролей пользователя

---
+++ Common/AntroStop.Domain.Base/Models/RolesInfo.cs
@@ -0,0 +1,14 @@
+﻿using AntroStop.Interfaces.Base.Entities;
+using System;
+
+namespace AntroStop.Domain.Base.Models
+{
+    public class RolesInfo : IIntEntity
+    {
+        public int ID { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
