b88804fd74d1c0b17f291a8a41fbd932a1f0477b
Александр Ягудин <yagudin.a@outlook.com>
2023-02-04 16:27:37 +0300

    Добавление библиотеки для постраничного обновления

---
+++ Common/AntroStop.Domain.Pagination/Features/PagingResponse.cs
@@ -0,0 +1,11 @@
+﻿using AntroStop.Domain.Pagination.RequestFeatures;
+using System.Collections.Generic;
+
+namespace AntroStop.Domain.Pagination.Features
+{
+    public class PagingResponse<T> where T : class
+    {
+        public List<T> Items { get; set; }
+        public MetaData MetaData { get; set; }
+    }
+}
---
+++ Common/AntroStop.Domain.Pagination/RequestFeatures/MetaData.cs
@@ -0,0 +1,30 @@
+﻿namespace AntroStop.Domain.Pagination.RequestFeatures
+{
+    public class MetaData
+    {
+        /// <summary>
+        /// Выбранная страница
+        /// </summary>
+        public int CurrentPage { get; set; }
+        /// <summary>
+        /// Всего страниц
+        /// </summary>
+        public int TotalPages { get; set; }
+        /// <summary>
+        /// Размер страницы
+        /// </summary>
+        public int PageSize { get; set; }
+        /// <summary>
+        /// Всего элементов
+        /// </summary>
+        public int TotalCount { get; set; }
+        /// <summary>
+        /// Отображать ли кнопку назад, если выбранная страница не первая
+        /// </summary>
+        public bool HasPrevious => CurrentPage > 1;
+        /// <summary>
+        /// Отображать ли кнопку вперед, если количество меньше чем всего страниц
+        /// </summary>
+        public bool HasNext => CurrentPage < TotalPages;
+    }
+}
