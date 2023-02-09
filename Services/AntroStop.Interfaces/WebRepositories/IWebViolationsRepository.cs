f4cc6ce387150dc66cdc9345b4e005927f2fba60
Александр Ягудин <yagudin.a@outlook.com>
2023-02-10 01:03:56 +0300

    Изменение репозитория для WebAPI Clients

---
+++ Services/AntroStop.Interfaces/WebRepositories/IWebViolationsRepository.cs
@@ -0,0 +1,23 @@
+﻿using AntroStop.Domain.Pagination.Features;
+using AntroStop.Domain.Pagination.RequestFeatures;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using System;
+
+namespace AntroStop.Interfaces.WebRepositories
+{
+    public interface IWebViolationsRepository<T> where T : class
+    {
+        Task<bool> ExistID(Guid ID, CancellationToken Cancel = default);
+        Task<int> Count(CancellationToken Cancel = default);
+        Task<IEnumerable<T>> GetAllByID(string Id, CancellationToken Cancel = default);
+        Task<IEnumerable<T>> GetAllByStatus(string Status, CancellationToken Cancel = default);
+        Task<int> GetCountByStatus(string Status, CancellationToken Cancel = default);
+        Task<T> Add(T entity, CancellationToken Cancel = default);
+        Task<T> Update(T entity, CancellationToken Cancel = default);
+        Task<T> Delete(Guid ID, CancellationToken Cancel = default);
+        Task<T> Get(Guid ID, CancellationToken Cancel = default);
+        Task<PagingResponse<T>> GetPage(PageParametrs productParameters, CancellationToken Cancel = default);
+    }
+}
