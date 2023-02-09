8ceb7b9ca80604d9cfdbaa89838af8866cb519fa
Александр Ягудин <yagudin.a@outlook.com>
2023-02-10 01:04:31 +0300

    Добавление нового интерфейса репозитория пользователя для дальнейшей регистрации

---
+++ Services/AntroStop.Interfaces/Repositories/IUsersRepository.cs
@@ -0,0 +1,12 @@
+﻿using AntroStop.Interfaces.Base.Entities;
+using AntroStop.Interfaces.Base.Repositories;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AntroStop.Interfaces.Repositories
+{
+    public interface IUsersRepository<T> : IStringRepository<T> where T :IStringEntity
+    {
+        Task<T> GetByData(string Id, string password, CancellationToken Cancel = default);
+    }
+}
