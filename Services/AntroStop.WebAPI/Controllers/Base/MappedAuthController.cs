a111f977f03598f4611e18899805acbe5c05fb07
Александр Ягудин <yagudin.a@outlook.com>
2023-02-10 01:08:11 +0300

    Добавление контроллера для авторизации

---
+++ Services/AntroStop.WebAPI/Controllers/Base/MappedAuthController.cs
@@ -0,0 +1,112 @@
+﻿using AntroStop.DAL.Entities;
+using AntroStop.Domain.Base.AuthModels;
+using AntroStop.Interfaces.Base.Repositories;
+using AntroStop.Interfaces.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntroStop.WebAPI.Controllers.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MappedAuthController: ControllerBase
+    {
+        
+        private readonly IConfiguration configuration;
+        private readonly IConfigurationSection jwtSettings;
+        private readonly IUsersRepository<User> repository;
+
+        public MappedAuthController(IUsersRepository<User> users, IConfiguration configuration)
+        {
+            this.repository = users;
+            this.configuration = configuration;
+            this.jwtSettings = this.configuration.GetSection("JwtSettings");
+        }
+
+        [HttpPost("Registration")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            if (userForRegistration == null || !ModelState.IsValid)
+                return BadRequest();
+
+            var user = new User
+            {
+                ID = userForRegistration.Id,
+                Name = userForRegistration.Name,
+                Password = userForRegistration.Password,
+                RoleID = 3,
+                Status = true
+            };
+
+            var result = await repository.Add(user);
+            if (result==null)
+                return BadRequest("Пользователь с таким E-mail уже существует");
+            
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UserForAuthenticationDto userForAuthentication)
+        {
+            var user = await repository.GetByData(userForAuthentication.Id, userForAuthentication.Password);
+
+            if (user == null)
+                return NotFound("Неверный E-mail или пароль");
+
+            if (!user.Status)
+                return Unauthorized("Ваш профиль заблокирован");
+
+            var signingCredentials = GetSigningCredentials();
+            var claims = GetClaims(user);
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+            return Ok(new AuthResponseDto { IsAuthSuccessful = true, Token = token });
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(jwtSettings.GetSection("securityKey").Value);
+            var secret = new SymmetricSecurityKey(key);
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private List<Claim> GetClaims(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.ID),
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Name),
+                new Claim(ClaimsIdentity.DefaultNameClaimType,user.CreatedAt.ToString()),
+                new Claim(ClaimsIdentity.DefaultNameClaimType,user.Role.Name)
+            };
+
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var tokenOptions = new JwtSecurityToken(
+                issuer: jwtSettings.GetSection("validIssuer").Value,
+                audience: jwtSettings.GetSection("validAudience").Value,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings.GetSection("expiryInMinutes").Value)),
+                signingCredentials: signingCredentials);
+
+            return tokenOptions;
+        }
+    }
+}
