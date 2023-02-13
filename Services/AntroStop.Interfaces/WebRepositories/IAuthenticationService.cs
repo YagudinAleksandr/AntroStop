27b0d917fd4e79b2dd0b9bbee548e3146cc65725
Александр Ягудин <yagudin.a@outlook.com>
2023-02-13 23:17:19 +0300

    Доработка интерфейса аутентификации

---
+++ Services/AntroStop.Interfaces/WebRepositories/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using AntroStop.Domain.Base.AuthModels;
+using System.Threading.Tasks;
+
+namespace AntroStop.Interfaces.WebRepositories
+{
+    public interface IAuthenticationService
+    {
+        Task<RegistrationResponseDto> RegisterUser(UserForRegistrationDto userForRegistration);
+        Task<AuthResponseDto> Login(UserForAuthenticationDto userForAuthentication);
+        Task Logout();
+    }
+}
