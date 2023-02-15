e7ee128f03429ebe6b5fe6d99c0b4bfbc172dcd8
Александр Ягудин <yagudin.a@outlook.com>
2023-02-16 01:02:28 +0300

    Создание сервиса авторизации

--- UI/AntroStop.MAUIUI/Services/LoginService.cs
+++ UI/AntroStop.MAUIUI/Services/LoginService.cs
@@ -1,13 +1,33 @@
 ﻿using AntroStop.Domain.Base.AuthModels;
 using AntroStop.Interfaces.WebRepositories;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace AntroStop.MAUIUI.Services
 {
     internal class LoginService : IAuthenticationService
     {
-        public Task<AuthResponseDto> Login(UserForAuthenticationDto userForAuthentication)
+        private readonly HttpClient client;
+        private readonly JsonSerializerOptions options;
+
+        public LoginService(HttpClient client)
         {
-            throw new NotImplementedException();
+            this.client = client;
+            this.options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<AuthResponseDto> Login(UserForAuthenticationDto userForAuthentication)
+        {
+            var content = JsonSerializer.Serialize(userForAuthentication);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var authResult = await client.PostAsync("api/MappedAuth/login", bodyContent);
+            var authContent = await authResult.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<AuthResponseDto>(authContent, options);
+            if (!authResult.IsSuccessStatusCode)
+                return result;
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Token);
+            return new AuthResponseDto { IsAuthSuccessful = true };
         }
 
         public Task Logout()
