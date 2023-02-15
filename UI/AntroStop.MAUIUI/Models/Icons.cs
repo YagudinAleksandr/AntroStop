606728e72421e541ebf14500e5769bfc151dab7a
Александр Ягудин <yagudin.a@outlook.com>
2023-02-16 01:01:59 +0300

    Добавление изображений и статического класса для работы с ними

---
+++ UI/AntroStop.MAUIUI/Models/Icons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntroStop.MAUIUI.Models
+{
+    public static class Icons
+    {
+        public static string Dashboard = "violation.png";
+        public static string Settings = "settings.png";
+        public static string Violations = "all_violations.png";
+    }
+}
