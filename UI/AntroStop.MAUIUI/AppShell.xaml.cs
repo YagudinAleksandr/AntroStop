8965ce7459cf2ac2871ac07f8061733bff1c1ea8
Александр Ягудин <yagudin.a@outlook.com>
2023-02-17 22:15:31 +0300

    Общие каркасные настройки

--- UI/AntroStop.MAUIUI/AppShell.xaml.cs
+++ UI/AntroStop.MAUIUI/AppShell.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace AntroStop.MAUIUI
+﻿using AntroStop.MAUIUI.ViewModels;
+using AntroStop.MAUIUI.Views.Dashboard;
+
+namespace AntroStop.MAUIUI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            this.BindingContext = new AppShellViewModel();
+
+            Routing.RegisterRoute(nameof(DashboardPage), typeof(DashboardPage));
         }
     }
 }
--- UI/AntroStop.MAUIUI/MauiProgram.cs
+++ UI/AntroStop.MAUIUI/MauiProgram.cs
@@ -1,4 +1,9 @@
-﻿namespace AntroStop.MAUIUI
+﻿using AntroStop.MAUIUI.ViewModels.Dashboard;
+using AntroStop.MAUIUI.ViewModels.Startup;
+using AntroStop.MAUIUI.Views.Dashboard;
+using AntroStop.MAUIUI.Views.Startup;
+
+namespace AntroStop.MAUIUI
 {
     public static class MauiProgram
     {
@@ -12,6 +17,15 @@
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            //Views
+            builder.Services.AddSingleton<LoginPage>();
+            builder.Services.AddSingleton<DashboardPage>();
+            
+
+            //Models
+            builder.Services.AddSingleton<LoginPageViewModel>();
+            builder.Services.AddSingleton<DashboardPageViewModel>();
 
             return builder.Build();
         }
