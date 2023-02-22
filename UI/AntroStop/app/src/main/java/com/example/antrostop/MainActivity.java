c09345ffdf1f5c8340c1730a3bcdb68385e28995
Александр Ягудин <yagudin.a@outlook.com>
2023-02-22 18:10:01 +0300

    Добавление элементов навигации

--- UI/AntroStop/app/src/main/java/com/example/antrostop/MainActivity.java
+++ UI/AntroStop/app/src/main/java/com/example/antrostop/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.antrostop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.antrostop.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+                case R.id.violations:
+                    replaceFragment(new ViolationsFragment());
+                    break;
+            }
+
+            return  true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+
+        fragmentTransaction.commit();
+
+
     }
 }
