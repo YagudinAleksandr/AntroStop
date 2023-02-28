b28eac91f3b6a0ab98d3d62559bce2db3c2efaf4
Александр Ягудин <yagudin.a@outlook.com>
2023-02-28 19:42:20 +0300

    Добавление MAUI Blazor Pages

---
+++ UI/AntroStop.MauiUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AntroStop.MauiUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
