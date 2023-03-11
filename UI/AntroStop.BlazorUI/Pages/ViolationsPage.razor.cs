aceca7d0345e33aa3d1d1f18129d36ce27dc348b
Александр Ягудин <yagudin.a@outlook.com>
2023-03-11 16:09:54 +0300

    Готовое отображение страницы заявок

---
+++ UI/AntroStop.BlazorUI/Pages/ViolationsPage.razor.cs
@@ -0,0 +1,39 @@
+﻿using AntroStop.Domain.Base.Models;
+using AntroStop.Domain.Pagination.RequestFeatures;
+using AntroStop.Interfaces.WebRepositories;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AntroStop.BlazorUI.Pages
+{
+    public partial class ViolationsPage
+    {
+        public List<ViolationsInfo> ViolationsList { get; set; } = new List<ViolationsInfo>();
+
+        public MetaData MetaData { get; set; } = new MetaData();
+
+        private PageParametrs _productParameters = new PageParametrs();
+
+        [Inject]
+        public IWebViolationsRepository<ViolationsInfo> ViolationsRepo { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await GetItems();
+        }
+
+        private async Task SelectedPage(int page)
+        {
+            _productParameters.PageNumber = page;
+            await GetItems();
+        }
+
+        private async Task GetItems()
+        {
+            var pagingResponse = await ViolationsRepo.GetPage(_productParameters);
+            ViolationsList = pagingResponse.Items;
+            MetaData = pagingResponse.MetaData;
+        }
+    }
+}
