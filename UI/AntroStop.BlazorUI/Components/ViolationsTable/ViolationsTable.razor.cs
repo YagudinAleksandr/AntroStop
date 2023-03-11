753d79d397796de9c8aef33e5013e353a461d262
Александр Ягудин <yagudin.a@outlook.com>
2023-03-11 16:10:17 +0300

    Реализация элементов таблицы отображения заявок

--- UI/AntroStop.BlazorUI/Components/ViolationsTable/ViolationsTable.razor.cs
+++ UI/AntroStop.BlazorUI/Components/ViolationsTable/ViolationsTable.razor.cs
@@ -1,4 +1,7 @@
 ﻿using AntroStop.Domain.Base.Models;
+using AntroStop.Interfaces.WebRepositories;
+using Blazored.Toast.Services;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 
@@ -8,5 +11,14 @@ namespace AntroStop.BlazorUI.Components.ViolationsTable
     {
         [Parameter]
         public List<ViolationsInfo> Violations { get; set; }
+
+        [Inject]
+        public IWebViolationsRepository<ViolationsInfo> ViolationsRepository { get; set; }
+
+        [Inject]
+        public IToastService toast { get; set; }
+        private ToastParameters _toastParameters = default!;
+
+
     }
 }
