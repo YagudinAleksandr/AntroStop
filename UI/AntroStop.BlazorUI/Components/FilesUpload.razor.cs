b0edf29fe7f04007a2f1876aeefa7843bd4e0cc0
Александр Ягудин <yagudin.a@outlook.com>
2023-05-08 15:33:50 +0300

    Загрузка файлов

---
+++ UI/AntroStop.BlazorUI/Components/FilesUpload.razor.cs
@@ -0,0 +1,42 @@
+﻿using AntroStop.Domain.Base.Models;
+using AntroStop.Interfaces.WebRepositories;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http.Headers;
+
+namespace AntroStop.BlazorUI.Components
+{
+    public partial class FilesUpload
+    {
+        [Parameter]
+        public string ImgUrl { get; set; }
+        [Parameter]
+        public EventCallback<string> OnChange { get; set; }
+        [Inject]
+        public IWebElementsRepository<ElementsInfo> repository { get; set; }
+
+        private async Task HandleSelected(InputFileChangeEventArgs e)
+        {
+            var imageFiles = e.GetMultipleFiles();
+            foreach (var imageFile in imageFiles)
+            {
+                if (imageFile != null)
+                {
+                    var resizedFile = await imageFile.RequestImageFileAsync("image/png", 300, 500);
+
+                    using (var ms = resizedFile.OpenReadStream(resizedFile.Size))
+                    {
+                        var content = new MultipartFormDataContent();
+                        content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data");
+                        content.Add(new StreamContent(ms, Convert.ToInt32(resizedFile.Size)), "image", imageFile.Name);
+                        ImgUrl = await repository.UploadProductImage(content);
+                        await OnChange.InvokeAsync(ImgUrl);
+                    }
+                }
+            }
+        }
+    }
+}
