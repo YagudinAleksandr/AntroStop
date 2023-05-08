d4e847f207b65eb7e5c234bf3ad3eb06e1450ef7
Александр Ягудин <yagudin.a@outlook.com>
2023-05-08 14:15:14 +0300

    Модель файла для общего пользования

---
+++ Common/AntroStop.Domain.Base/Models/ElementsInfo.cs
@@ -0,0 +1,14 @@
+﻿using AntroStop.Interfaces.Base.Entities;
+using System;
+
+namespace AntroStop.Domain.Base.Models
+{
+    public class ElementsInfo : IGuidEntity
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public Guid ViolationID { get; set; }
+        public string FileName { get; set; }
+    }
+}
